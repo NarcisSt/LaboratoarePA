4a791072e0c513d273762ad9e0c299dd3e4bad9f
NarcisSt <75793612+NarcisSt@users.noreply.github.com>
2021-04-27 11:44:29 +0300

    Added Lab9 Compulsory 99%, without javadoc

---
+++ Lab9/src/main/java/CompulsoryLab9/EntityClasses/Genres.java
@@ -0,0 +1,48 @@
+package CompulsoryLab9.EntityClasses;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+import lombok.experimental.Accessors;
+
+import javax.persistence.*;
+
+@ToString
+@EqualsAndHashCode
+@Accessors(chain = true)
+@Data
+@Entity
+@Table(name = "GENRES")
+public class Genres {
+    @Id
+    @Column(name = "ID", nullable = false)
+    private Long id;
+    @Column(name = "NAME")
+    private String name;
+
+    public Genres(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Genres() {
+
+    }
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
---
+++ Lab9/src/main/java/CompulsoryLab9/RepoClasses/GenresRepo.java
@@ -0,0 +1,21 @@
+package CompulsoryLab9.RepoClasses;
+
+import CompulsoryLab9.EntityClasses.Genres;
+import CompulsoryLab9.Singleton.PersistenceManager;
+
+import javax.persistence.EntityManager;
+
+public class GenresRepo {
+    public Genres findGenreByName(String name) {
+        EntityManager entityManager = PersistenceManager.INSTANCE.getEntityManager();
+        return (Genres) entityManager.createQuery("SELECT g FROM Genres g WHERE g.id=?1").setParameter(1, name).getSingleResult();
+    }
+
+    public void create (Genres genres) {
+        EntityManager entityManager = PersistenceManager.INSTANCE.getEntityManager();
+
+        entityManager.getTransaction().begin();
+        entityManager.persist(genres);
+        entityManager.getTransaction().commit();
+    }
+}
