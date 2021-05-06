f8e64f3d536b90b100c32a7f5fb73825def5f0f8
Coteanu Maria <maria.coteanu@info.uaic.ro>
2021-05-06 19:21:40 +0300

    modified modules

---
+++ PALab10/Lab10Server/src/main/java/serverCommunication/ClientThread.java
@@ -0,0 +1,91 @@
+package serverCommunication;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class ClientThread extends Thread {
+    private Socket socket;
+    private Server server;
+
+    public ClientThread(Socket socket, Server server) {
+        this.socket = socket;
+        this.server = server;
+    }
+
+    public void run() {
+        try {
+            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            while (true) {
+                String request = in.readLine();
+                String[] command = request.split(" ", 2);
+                if (command[0].equalsIgnoreCase("stop")) {
+                    String raspuns = "Server stopped";
+                    PrintWriter out = new PrintWriter(socket.getOutputStream());
+                    out.println(raspuns);
+                    out.flush();
+                    server.sSocket.close();
+                } else {
+                    processCommand(command);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                socket.close();
+            } catch (IOException e) {
+                System.err.println(e);
+            }
+        }
+    }
+
+    private void processCommand(String[] command) {
+        try {
+            switch (command[0]) {
+                case "register":
+                    if (server.people.contains(command[1])) {
+                        String raspuns = "User exists";
+                        PrintWriter out = new PrintWriter(socket.getOutputStream());
+                        out.println(raspuns);
+                        out.flush();
+                    }
+                    else{
+                        server.people.add(command[1]);
+                        String raspuns = "User created";
+                        PrintWriter out = new PrintWriter(socket.getOutputStream());
+                        out.println(raspuns);
+                        out.flush();
+                    }
+                    break;
+                case "login":
+                    if (server.people.contains(command[1])) {
+                        String raspuns = "Logged In";
+                        PrintWriter out = new PrintWriter(socket.getOutputStream());
+                        out.println(raspuns);
+                        out.flush();
+                    }
+                    else{
+                        server.people.add(command[1]);
+                        String raspuns = "Please register";
+                        PrintWriter out = new PrintWriter(socket.getOutputStream());
+                        out.println(raspuns);
+                        out.flush();
+                    }
+                    break;
+                case "friend":
+                    break;
+                case "send":
+                    break;
+                case "read":
+                    break;
+                default:
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
--- PALab10/Lab10Server/src/main/java/serverCommunication/Server.java
+++ PALab10/Lab10Server/src/main/java/serverCommunication/Server.java
@@ -3,19 +3,25 @@ package serverCommunication;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
 
 public class Server {
     public static final int PORT = 8100;
-
+    protected ServerSocket sSocket;
+    protected List<String> people = new LinkedList<>();
+    protected Map<String,List<String>> friendship = new HashMap<>();
     public Server() {
-        ServerSocket sSocket;
         try {
             sSocket = new ServerSocket(PORT);
             while (true) {
                 System.out.println("Waiting for a client ...");
                 Socket socket = sSocket.accept();
                 // Execute the client's request in a new thread
-                new ClientThread(socket).start();
+                new ClientThread(socket, this).start();
             }
 
         } catch (IOException e) {
